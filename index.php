5cf842ab029bbf5f0364bb93d0cdb84b6bdd07ef
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-04 09:53:20 +0800

    Initial commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>mySPBT-Home</title>
-	<link rel="icon" href="#" type="image/png">
+	<link rel="icon" href="img/favicon_myspbt" type="image/png">
 
   <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
   <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
